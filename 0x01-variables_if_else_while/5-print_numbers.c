86a81602e9e5033d3cf3de9b28ebf94faf2f9966
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-14 16:25:39 -0700

    Variables and loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description : prints aphabets in lowercase, uses putcha
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	/* body of code*/
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
