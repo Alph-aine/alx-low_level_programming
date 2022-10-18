3881279315775df8b94e8e60d546ac032ebf7e15
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-18 16:39:30 -0700

    Function and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs- An absolute number function
+ * @int: the parameter / data type of argument
+ *
+ * Description: It returns the absolute valu of any number
+ * Return: 0 (success)
+ */
+int _abs(int)
+{
+	int = +*int
+	_putchar(int);
+	return (0);
+}
+
