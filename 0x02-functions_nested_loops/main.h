fac01f78fd6c6c7aab33a8e84e42db5551463678
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-18 13:03:30 -0700

    function and loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 /**
  * _putchar : This prints to the stdout
- * @char input: the data type of the parameter
+ * @char a : the data type of the parameter
  *
  * Description: A prototype for putchar function
  * Return: Always 0 (Success)
  */
-int  _putchar(char input);
+int  _putchar(char c);
