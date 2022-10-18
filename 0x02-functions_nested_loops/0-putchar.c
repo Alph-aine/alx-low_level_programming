595150534d96bab047554b40a216ea31144cd1ce
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-18 13:17:01 -0700

    Function and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,14 @@
   */
 int main(void)
 {
-	_putchar('_putchar\n');
+	int i;
+	char a[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,1 @@
-/**
- * _putchar : This prints to the stdout
- * @char a : the data type of the parameter
- *
- * Description: A prototype for putchar function
- * Return: Always 0 (Success)
- */
-char _putchar(char c);
+int  _putchar(char c);
