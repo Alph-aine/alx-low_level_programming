5d7c81151f5aacf91b780a8dc1d29cf07fe54a53
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-18 18:32:26 -0700

    Function and loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *@x : first parameter
+ *@y: second parameter
+ *
+ * Description: This function takes 2 arguments for addition
+ * Return: Always 0
+ */
+int add(int x, int y)
+{
+	int n;
+
+	n = x + y;
+	_putchar(n);
+	return (0);
+}
