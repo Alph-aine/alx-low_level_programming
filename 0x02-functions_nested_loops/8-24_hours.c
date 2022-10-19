f6d40eabbe9d0012a893b8a38f3435fd0da8608c
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-18 17:46:10 -0700

    Function and loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,12 +6,22 @@
  */
 void jack_bauer(void)
 {
-	int i;
+	int i, j;
 
-	i = '00:00'
-	for (i = '00:00'; i <= '23:59'; i++)
+	i = 0;
+	while (i < 24)
 	{
-		_putchar(i);
-		return (0);
+		j = 0;
+		while (j < 60)
+		{
+			_putchar(i % 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
 	}
 }
