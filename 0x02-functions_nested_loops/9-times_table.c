472a9cc21baf767ade6d13b719998de1fa736d9f
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-18 18:06:58 -0700

    Function and loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - prints time table from 0-9
+ * Description: uses while loop
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i, j;
+
+	i = 0;
+	while (1 <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			_putchar(j * i);
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+		i++;
+	}
+
+}
