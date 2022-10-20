260bd2b11cc2582c6f822215bd4530dbb63c3d4a
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-20 15:51:58 -0700

    Functions and loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry pointof the program
+ * Description: prints the first 50 fibonnacci number
+ * Return: 0
+ */
+int main(void)
+{
+	int i, a, b, c;
+
+	a = 0;
+	b = 1;
+	for (i = 1; i <= 98; i++)
+	{
+		c = a + b;
+		printf("%d", c);
+		if (i != 50)
+		{
+			printf(",");
+			printf(" ");
+		}
+		a = b;
+		b = c;
+	}
+	return (0);
+}
