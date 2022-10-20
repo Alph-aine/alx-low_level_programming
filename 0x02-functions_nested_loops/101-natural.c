2d496c075b6a671a9bebe269d746af0ee35ac8eb
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-20 08:17:58 -0700

    Functions and loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -16,8 +16,7 @@ int main(void)
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
-			printf("The sum of the multiples of 3 and 5 below 1024
-					is %d\n", sum);
+			printf("%d\n", sum);
 		}
 	}
 	return (0);
