4074008839a9055b41b2eaa61bcd1b35c9e2fd89
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-20 17:40:04 -0700

    Debugging

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isupper- A function to check for uppercase letters
+ *@c: parameter that takes the argumnet
+ *
+ * Description: function fpr checking uppercase letters
+ * Return: 1 0r 0
+ */
+int _isupper(int c)
+{
+	if (c == 'A' || c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
