4fe0309a9c235609528b4beca164f5c9fc8db497
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-20 18:21:12 -0700

    More functions and loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 
 	i = 0;
 	j = 0;
-	while (j <= 10)
+	for (j = 0; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
@@ -20,6 +20,5 @@ void more_numbers(void)
 			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
-		j++;
 	}
 }
