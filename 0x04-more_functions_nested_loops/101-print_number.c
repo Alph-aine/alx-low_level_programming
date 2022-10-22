a2647f7ab2e160d2632d7223b159fbec8e3f22a1
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-21 17:54:57 -0700

    More functions and loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,13 +10,13 @@ void print_number(int n)
 {
 	if ( n == 0)
 		_putchar('0');
-	if (n < 0)
+	else if(n < 0)
 	{
 		n = n * -1;
 		_putchar('-');
 		_putchar(n + '0');
 	}
-	if (n < 10)
+	else if (n < 10)
 		_putchar(n + '0');
 	else if (n >= 100 ||  n < 100)
 	{
