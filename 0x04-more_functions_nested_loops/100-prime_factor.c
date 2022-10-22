7f585a509765fb4ec002c559758c801abc8aacad
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-21 17:19:58 -0700

    More functions and loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,19 +14,16 @@ int main(void)
 	n = 612852475143;
 	while (n % 2 == 0)
 	{
-	/*	printf("%d ", 2);*/
 		n = n / 2;
 	}
 	for (i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
 		{
-			/*printf("%d ", i);*/
 			n = n / i;
 		}
 	}
 	if (n > 2)
-		printf("%ld ", n);
-	printf("\n");
+		printf("%ld\n", n);
 	return (0);
 }
