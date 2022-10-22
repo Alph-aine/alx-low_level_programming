fea129a7ca9e6d68d6734e4d2527c871c31a3b9c
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-21 17:37:13 -0700

    More functions and loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,5 +8,27 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	}
+	else
+		m = n;
+	if (m < 10)
+		_putchar(m + '0');
+	else if (m >= 100 && m < 100)
+	{
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+	else if (m >= 100 && m < 1000)
+	{
+		_putchar((m / 100) + '0');
+		_putchar(((m / 10) % 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+	_putchar('\n');
 }
