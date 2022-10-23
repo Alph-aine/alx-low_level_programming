457926c52cfd51b0293d46a7af1bc5ee0b56f57d
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-23 00:14:00 -0700

    Mre function and loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,25 +8,28 @@
  */
 void print_number(int n)
 {
+	int m;
+
+	n = m;
 	if (n == 0)
 		_putchar('0');
 	else if (n < 0)
 	{
-		n = n * -1;
+		m = n * -1;
 		_putchar('-');
-		_putchar(n + '0');
+		_putchar(m + '0');
 	}
-	else if (n < 10)
-		_putchar(n + '0');
-	else if (n >= 10 &&  n < 100)
+	else if (m < 10)
+		_putchar(m + '0');
+	else if (m >= 10 &&  m < 100)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
 	}
-	else if (n >= 100 && n < 1000)
+	else if (m >= 100 && m < 1000)
 	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
+		_putchar((m / 100) + '0');
+		_putchar(((m / 10) % 10) + '0');
+		_putchar((m % 10) + '0');
 	}
 }
