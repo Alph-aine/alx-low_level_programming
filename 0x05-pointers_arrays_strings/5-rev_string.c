7c89295ce7af133e905adcd633f944ef8e74749a
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-24 15:38:45 -0700

    C-pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *rev_string-- reverses a strng
+ *@s: the stringr
+ *
+ * Return: the length of the string
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		s += s[i];
+	}
+	*s = s;
+}
