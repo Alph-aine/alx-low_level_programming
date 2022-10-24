0f8fb44abfac01176408cdbe26b51d5516690d63
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-24 16:07:15 -0700

    C-pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,15 @@
  */
 void rev_string(char *s)
 {
-	int i, len, temp;
+	int i;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		len++;
+		i++;
 	}
 	for (i = 0; i < len / 2; i++)
 	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		*s += s[i];
 	}
 }
