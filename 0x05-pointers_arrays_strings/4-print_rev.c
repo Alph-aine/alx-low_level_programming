918fe67cc834ad28cb6371398c1a56517381484c
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-24 15:32:31 -0700

    C-pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,15 @@ void print_rev(char *s)
 {
 	int i;
 
-	i = 0;
-	while (*s != '\0')
+	i = 0
+	while (s[i] != '\0')
 	{
-		s++;
 		i++;
 	}
-	while (i >= 0)
+	for (i = i - 1; i >= 0; i--)
 	{
-		_putchar(s['i']);
-		i--;
+		_putchar(s[i]);
 	}
+
 	_putchar('\n');
 }
