960b43e6f554f8bcc4eb8316d50a1601721fc93f
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-24 09:11:06 -0700

    C-pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *swap_int- swaps the value of two variables via their memory address
+ *@a: first argument variable
+ *@b: scond argument var
+ *
+ *Return: none
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
