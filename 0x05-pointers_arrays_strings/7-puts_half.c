be15b303d13c1aafa58d136ece3106d13bbcf813
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-25 14:19:01 -0700

    C-pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *puts_half- prints every other character of a string
+ *@str: the string
+ *
+ * Return: the length of the string
+ */
+void puts_half(char *str)
+{
+	int i, j, n, k, len;
+	char temp;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i  / 2;
+	if (j > len)
+	{
+		for (j = 0; j < i; j++)
+		{
+			temp = str[j];
+			k++;
+		}
+		/*if (k % 2 != 0)*/
+		/*{*/
+/*		}*/
+		/*n = (i - 1) / 2; */
+	}
+}
