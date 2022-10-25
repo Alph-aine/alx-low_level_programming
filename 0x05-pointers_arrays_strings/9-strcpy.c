ed0dc9e970180f0860ad9276424902cc6e75782e
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-25 16:38:32 -0700

    C-pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strcpy- copies a stirng form a variable to another
+ *@dest: the destination
+ *@src: the source
+ *
+ *Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (*dest);
+}
