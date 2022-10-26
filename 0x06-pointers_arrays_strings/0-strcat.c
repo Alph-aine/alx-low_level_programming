3a9e4b329c31c6a0f8a3245658ec52fb01eec9c4
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-26 08:07:29 -0700

    More pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+/**
+ *_strcat- concatenates the second parameter to the first
+ *@dest: first parameter
+ *@src: second parameter
+ *
+ *Return: dest
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i, j, len;
 
 	i = 0;
 	len = 0;
-	while ( dest[len] != '\0')
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
