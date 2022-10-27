1b456382c01e009c253f7180b49a46592d8c805a
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-27 14:59:31 -0700

    More pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *string_toupper- returns the uppercase of letters
+ *@str: the parameter to be passed- string
+ *
+ *Return: str
+ */
+char *string_toupper(char *str)
+{
+	int  i;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
