c9edaf96cc2ca9fcf386b3bd903261a97383d75a
Abdulbasit <aabdulrafiu721@gmail.com>
2022-10-27 10:24:51 -0700

    More pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,10 @@
  */
 void reverse_array(int *a, int n)
 {
-	int length, i, last_pos;
+	int i, last_pos;
 	char temp;
 
 	i = 0;
-	length = sizeof(a) / sizeof(a[0]);
-	n = length;
 	last_pos = n - 1;
 
 	for (i = 0; i < n / 2; i++)
