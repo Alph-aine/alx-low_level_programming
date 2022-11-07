6b93c722f8e3f85487b5b6e70c5f5ad3dceb93a4
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-07 12:59:47 -0800

    More Pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/** 
+/**
  *_memset- replaces characters of a string with a givn character n times
  *@s:  the string
  *@b: the character
@@ -9,7 +9,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i; 
+	int i;
 
 	i = 0;
 	for (i = 0; i < n; i++)
