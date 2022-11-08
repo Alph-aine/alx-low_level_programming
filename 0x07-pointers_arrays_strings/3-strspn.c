17810045f2ee9a69781f7fcab1c0eb2fd7c75baf
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-08 14:03:19 -0800

    More Pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,13 +9,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
+	int k = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == accept[i])
+		j = 0;
+		while (accept != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				break;
+			}
 			j++;
+		}
 		i++;
 	}
-	return (j);
+	return (k);
 }
