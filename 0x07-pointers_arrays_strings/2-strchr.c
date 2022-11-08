d2c1750a695fa994a9222c96538d12d12bc5efaf
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-08 13:15:46 -0800

    More Pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return (&s[i]);
-		else if (c == '\0')
-			return (NULL);
+	while (s[i] != '\0' && s[i] != c)
 		i++;
-	}
-	return (0);
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
 }
