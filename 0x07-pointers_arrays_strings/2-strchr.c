32736065aa9d86d393a7b1c6eeed6a02fd90a9e2
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-07 17:44:31 -0800

    More Pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strchr- searches for a character in a string
+ *@s: the string
+ *@c: the character
+ *
+ *Return: a pointer to c or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+}
