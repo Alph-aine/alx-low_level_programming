a6aa33e3bdb9b83db6dbebf78cb515887fc74f70
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-08 14:33:50 -0800

    More Pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strpbrk- cheecks for the first match betwee
+ *two strings
+ *
+ *@s: first string
+ *@accept: second string
+ *Return: the first match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	int k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k = s[i];
+				break;
+			}
+			else
+				k = NULL;
+		}
+	}
+	return (k);
+}
