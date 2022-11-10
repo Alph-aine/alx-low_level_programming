6ffcb6cc719a7a819a34fc17cef0275b3d61ed06
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-10 13:17:38 -0800

    C-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,22 +7,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i, j, k;
-
-	i = j = k = 0;
-	while (s[i] != '\0')
-		i++;
-	k = i;
-	while (i >= 0)
+	if (*s == '\0')
+		return;
+	if (*s != '\0')
 	{
-		if (j == k)
-		{
-			_putchar(s[i]);
-			return;
-		}
-		_putchar(s[i]);
-		i--;
-		j++;
+		_print_rev_recursion(s + 1);
+		s++;
+		_putchar(*s);
 	}
-	_print_rev_recursion(s);
 }
