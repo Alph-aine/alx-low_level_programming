05c74f8b937d0cc0a9c38558f52de5a6ce7a662e
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-14 18:00:33 -0800

    Unused variables

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,13 +7,13 @@
  *Return: 0 or 1
  */
 
-int main(int argc, __attribute__ ((unused)) int *argv[])
+int main(int argc, __attribute__ ((unused)) char *argv[])
 {
 	int i = 1;
 	int sum = 0;
 
 	for (; i <= argc; i++)
-		sum += argv[i];
+		sum += *argv[i];
 
 	if (argc > 2)
 		printf("%d\n", sum);
