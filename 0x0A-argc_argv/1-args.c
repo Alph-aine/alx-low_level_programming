88f45bed4a042b35f47d9d49e28060305ec12596
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-14 17:32:24 -0800

    argc/argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ *main- entry point into the  program
+ *@argc: the number of arguments passed while executing the program
+ *@argv:the valus of those arguments
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
