29babb252f5cbfe7a8684adeaebb27ddec9446db
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-15 18:27:35 -0800

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *str_concat- joins s1 & s2 into a new variable
+ *@s1: the first string
+ *@s2: the second string
+ *
+ *Return: a new string or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *join;
+	int i = j = len = 0;
+	int k, l;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (*(s1 + i))
+		i++;
+	i++; /*incremented to include null terminator*/
+	while (*(s2 + j))
+		j++;
+	j++;
+	len = i + j;
+
+	join = malloc(sizeof(char) * len);
+	if (join == NULL)
+		return (NULL);
+	while (k < i)
+	{
+		*(join + k) = *(s1 + k);
+		k++;
+	}
+	while (l < j)
+	{
+		*(join + l) = *(s2 + l);
+		j++;
+	}
+	*(join + len) = '\0';
+	return (join);
+}
