f0519c01cb83e72d074e79b9ed80cd143dbfdd64
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-17 18:03:33 +0000

    struct header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog- creates a struct namd dog
+  *@name: first memeber, a pointer
+  *@age: second member
+  *@owner: third memeber, a pointer
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
