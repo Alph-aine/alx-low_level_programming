068ed2bb30bcdb71237ecac676a8569eecdcc37a
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-17 20:47:33 +0000

    new struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+  *new_dog-creates a new dog struct
+  *@name: the name
+  *@age: the age
+  *@owner: the owner
+  *Return: NULL if it fails / a new struct if succesful
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *copy;
+
+	copy = malloc(sizeof(dog_t));
+	if (copy == NULL)
+		return (NULL);
+	copy->name = name;
+	copy->age = age;
+	copy->owner = owner;
+	return (copy);
+}
