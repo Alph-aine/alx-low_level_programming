32436215a1eeee3cdc1e57aea0462268b95bda96
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-16 21:11:26 -0800

    checking

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,15 +12,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	int temp;
+	/*int temp;*/
 
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
 	for (; i < size ; i++)
 	{
-		temp = cmp(*(array + i);
-		if (temp)
+		/*temp = cmp(*(array + i);*/
+		if (cmp(*(array + i))
 			return (i);
 	}
 	return (-1);
