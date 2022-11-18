e7fd91d8f4a885ace125d6530811bb7106753ef4
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-17 20:03:10 -0800

    fix errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "3-calc.h"
+#include <stdlib.h>
 /**
  *main- entry point into the program
  *@argc: the number of arguments passed on execution
@@ -27,12 +28,12 @@ int main(int argc, char *argv[])
 	n2 = atoi(argv[3]);
 
 	f = get_op_func(argv[2]);
-	if (f == NULL)
+	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2] == '/' || argv[2] == '%') && argv[3] == '0')
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
 	{
 		printf("Error\n");
 		exit(100);
