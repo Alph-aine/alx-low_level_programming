ee57b3d783b8639fae7a302e04a6db5348f513ff
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-19 14:43:05 +0000

    Fixed header guards

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
-#ifndef VARIADIC_FUNCTIONS_H /*variadic_function.h*/
+#ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 
 
-#endif /* variadic_function.h*/
+#endif
