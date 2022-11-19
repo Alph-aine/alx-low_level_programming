fc68c9d68826b5224b108ff3bdbbe281ec31297b
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-19 15:10:25 +0000

    Prints formatted numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include  <stdarg.h>
+#include <stdio.h>
+/**
+  *print_numbers-print numbers with a separator
+  *@separator: a character (',')
+  *@n: the number of arguments
+  *
+  *Return: none
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, x;
+	va_list  args;
+
+	if (separator == NULL)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		printf("%d", x);
+		printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
