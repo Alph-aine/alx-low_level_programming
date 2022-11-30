d6f68b47559322e7be2ecf4775a0f6d40a855d74
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-30 10:21:39 +0000

    executes before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
   *before_main- executes before main
   */
-void before_main(void) __attribute__((constructor))
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my
 			back!\n");
