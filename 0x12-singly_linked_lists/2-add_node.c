45786cc02c99e862301316ba07cff8ee1c2bd013
Abdulbasit <aabdulrafiu721@gmail.com>
2022-11-30 08:36:23 +0000

    Initial commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,60 @@
+#include "lists.h"
+#include <string.h>
+/**
+ *_strlen- finds the length of a string
+ *@str: the string
+ *Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ *add_node- adds a node to the beginning of a list
+ *@head: the existing list
+ *@str: the string in it
+ *Return: a pointer the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+	if (strdup(str) == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = str;
+	new_node->len = _strlen(str);
+
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
