d9db35d2aa7bf8cf6470b9da1bd487c0e83e1bee
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-01 18:20:39 +0000

    Removed assigning head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,13 +5,16 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
+
 	if (head == NULL)
 		return;
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		head->n = NULL;
-		head = NULL;
-		head = head->next;
+		temp = *head;
+		*head = temp->next;
+		free(temp);
 	}
 
 }
