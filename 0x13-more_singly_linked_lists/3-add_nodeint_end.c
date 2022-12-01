400578e5e1ba25934169a99afd4e503b50a38fab
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-01 08:57:20 -0800

    Error fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 	else
 	{
-		temp = *head
+		temp = *head;
 		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new_node;
