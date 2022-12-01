5fa2dde882d5d00eb660f620125b1f310117336e
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-01 17:20:16 +0000

    Initial commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ *free_listint-frees a node
+ *@head: the node
+ */
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+}
