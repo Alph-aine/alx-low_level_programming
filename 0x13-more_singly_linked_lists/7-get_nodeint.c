61ca484b52cdcad5d5aa754ffb418c847abb211a
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-01 19:03:00 +0000

    Error fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,13 +12,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	count = 0;
 	if (head == NULL)
 		return (NULL);
-	while ((count < index) && temp != NULL)
+
+	while ((count < index) && head != NULL)
 	{
 		if (count == index)
 			return (head);
 		count++;
 		head = head->next;
 	}
-	
 	return (NULL);
 }
