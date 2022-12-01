09a53df8a8a823039d459dd4f023ce6fd022e36f
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-01 18:12:37 +0000

    Initial commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *free_listint2- frees a list by setting it to null
+ *@head: the list
+ */
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+		return;
+	head = NULL;
+}
