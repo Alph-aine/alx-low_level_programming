9cec3f04db31485f4747273ad889b927e28aedd5
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-03 02:14:16 +0000

    Initial commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *get_bit- returns the bit at an index
+ *@n: the number
+ *@index: the index
+ *Return: -1 or the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int len = 0, bit;
+	unsigned long int n_copy = n;
+
+	while (n_copy > 0)
+	{
+		len++;
+		n_copy >>= 1;
+	}
+	if (index > len)
+		return (-1);
+	bit = ((n >> index) & 1);
+	return (bit);
+}
