7bcf49a798c42431c8991c0ec1baa875c64ba5c1
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-03 01:41:36 +0000

    Initial commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *print_binary- converts decimal to binary
+ *@n: the decimal
+ *Return: none
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int n_copy = n;
+	int  len = 0;
+
+	while (n_copy > 0)
+	{
+		len++;
+		n_copy >>= 1;
+	}
+	while (len >= 0)
+	{
+		if ((n >> len) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+		len--;
+	}
+}
