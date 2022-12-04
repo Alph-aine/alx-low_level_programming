5dc5c759f7f12e61b65120b6f5a5f1bfc50a1309
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-04 00:48:15 +0000

    Initial commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/* if idx is 0 */
 	if (idx == 0)
 	{
-		new_mode->next = *head;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
