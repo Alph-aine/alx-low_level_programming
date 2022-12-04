fe47cfef56cce237861166f7f1148117ad1a2017
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-04 01:27:48 +0000

    Stack error fixe

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -41,7 +41,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	/* set the index to be deleted to a temp */
 	temp2 = temp->next;
-	temp = temp2->next;
+	temp->next = temp2->next;
 	free(temp2);
 	return (1);
 }
